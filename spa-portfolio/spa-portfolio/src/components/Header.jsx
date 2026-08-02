cb8f0b72e5f5d0dddd0e8227df7c74a7eb1b77f6
faithkyungu-fnk <fnkyungu@gmail.com>
2026-08-02 03:48:00 +0300

    data and componenets

---
+++ spa-portfolio/spa-portfolio/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <header className="header">
+            <h1>Creative Portfolio</h1>
+            <p>Showcasing our latest web development projects.</p>
+        </header>
+    );
+}
+export default Header;
---
+++ spa-portfolio/spa-portfolio/src/data/projects.js
@@ -0,0 +1,16 @@
+const projects = [
+    {
+        id:1,
+        title:"Resturant Website",
+        description:"A responsive website for a local resturant."
+        image:
+         technology:"React, CSS",
+    },
+
+{
+    id:2,
+    title:"Portfolio website"
+    description: "A personal portfolio showcasingwe development projects."
+    image:
+    technology: "React,JavaScript"
+}
